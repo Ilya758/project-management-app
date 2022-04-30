6b735b44607a2de8b4bf0ffead2c9f802372f069
Ilya758 <subscriber6436@gmail.com>
2022-04-30 16:28:53 +0300

    feat: implemented basic router structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
+import BoardPage from './components/BoardPage/BoardPage';
+import HomePage from './components/HomePage/HomePage';
+import LoginPage from './components/LoginPage/LoginPage';
+import MainPage from './components/MainPage/MainPage';
+import NotFoundPage from './components/NotFoundPage/NotFoundPage';
+import WelcomePage from './components/WelcomePage/WelcomePage';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="" element={<HomePage />}>
+          <Route path="" element={<WelcomePage />}></Route>
+
+          <Route path="login" element={<LoginPage />}></Route>
+
+          <Route path="main" element={<MainPage />}></Route>
+
+          <Route path="board" element={<BoardPage />}></Route>
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/BoardPage/BoardPage.tsx
@@ -0,0 +1,5 @@
+const BoardPage = () => {
+  return <div>BoardPage</div>;
+};
+
+export default BoardPage;
