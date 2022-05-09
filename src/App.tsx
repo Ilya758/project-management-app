8a7c66c6a133cae56370e4e921edc0e4742d4122
tyzikova <tyzikova27@mail.ru>
2022-05-09 12:51:11 +0300

    fix:correct notes teamLead

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,57 @@
-import Routes from './Routes';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { store } from './store/store';
-import { ButtonUp } from './components/ButtonUp/ButtonUp';
+// import Routes from './Routes';
+// import { Provider } from 'react-redux';
+// import { BrowserRouter } from 'react-router-dom';
+// import { store } from './store/store';
+// import { ButtonUp } from './components/ButtonUp/ButtonUp';
+
+// const App = () => {
+//   return (
+//     <Provider store={store}>
+//       <BrowserRouter>
+//         <Routes />
+//         <ButtonUp />
+//       </BrowserRouter>
+//     </Provider>
+//   );
+// };
+
+// export default App;
+
+import { Navigate, Route, Routes } from 'react-router-dom';
+import BoardPage from './components/BoardPage/BoardPage';
+import HomePage from './components/HomePage/HomePage';
+import LoginPage from './components/LoginPage/LoginPage';
+import { Authentication } from './components/Authentication/Authentication';
+import MainPage from './components/MainPage/MainPage';
+import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
+import { WelcomePage } from './components/WelcomePage/WelcomePage';
+import { Login } from './components/Authentication/Login';
+import { Registration } from './components/Authentication/Registration';
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes />
-        <ButtonUp />
-      </BrowserRouter>
-    </Provider>
+    <>
+      <Routes>
+        <Route path="" element={<HomePage />}>
+          <Route index element={<WelcomePage />}></Route>
+
+          <Route path="login" element={<LoginPage />}></Route>
+
+          <Route path="authentication" element={<Authentication />}>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="registration" element={<Registration />}></Route>
+          </Route>
+
+          <Route path="main" element={<MainPage />}></Route>
+
+          <Route path="board" element={<BoardPage />}></Route>
+
+          <Route path="404" element={<NotFoundPage />} />
+
+          <Route path="*" element={<Navigate to="404" />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { store } from './store/store';
 import './index.css';
+import { ButtonUp } from './components/ButtonUp/ButtonUp';
 
 const container = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(container);
 
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <ButtonUp />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
