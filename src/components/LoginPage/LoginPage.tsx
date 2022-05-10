88b289a2567448a183dbcc57f03be0942002d609
inord777dev <inord777dev@gmail.com>
2022-05-11 00:22:25 +0300

    feat: add material UI

--- src/components/LoginPage/LoginPage.tsx
+++ src/components/LoginPage/LoginPage.tsx
@@ -1,15 +1,17 @@
 import { useState } from 'react';
 import authService from '../../services/services.auth';
 import { StyledLoginPage } from './LoginPages.styles';
+import Button from '@mui/material/Button';
+import { Container, TextField, Typography } from '@mui/material';
 
 const LoginPage = () => {
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleOnChangeLogin = (e: React.FormEvent<HTMLInputElement>) => {
+  const handleOnChangeLogin = (e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setLogin(e.currentTarget.value as string);
   };
-  const handleOnChangePassword = (e: React.FormEvent<HTMLInputElement>) => {
+  const handleOnChangePassword = (e: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     setPassword(e.currentTarget.value as string);
   };
 
@@ -24,43 +26,39 @@ const LoginPage = () => {
   };
 
   return (
-    <StyledLoginPage>
-      <div className="container">
-        <div className="title">Login to your account</div>
-        <form className="form" onSubmit={handerSubmit}>
-          <div className="form__row">
-            <div className="form__row-label">User</div>
-            <div className="form__row-input">
-              <input
-                className="form__input"
-                type="text"
-                value={login}
-                onChange={handleOnChangeLogin}
-              />
+    <Container maxWidth="sm">
+      <StyledLoginPage>
+        <div className="container">
+          <Typography variant="h5" component="h3">
+            Login to your account
+          </Typography>
+          <form className="form" onSubmit={handerSubmit}>
+            <div className="form__row">
+              <div className="form__row-input">
+                <TextField label="Login" type="text" value={login} onChange={handleOnChangeLogin} />
+              </div>
             </div>
-          </div>
-          <div className="form__row">
-            <div className="form__row-label">Password</div>
-            <div className="form__row-input">
-              <input
-                className="form__input"
-                type="text"
+            <div className="form__row">
+              <TextField
+                label="Password"
+                type="password"
                 value={password}
                 onChange={handleOnChangePassword}
               />
             </div>
-          </div>
-          <div className="form__row form__row-fotter">
-            <input
-              className="form__submit"
-              type="submit"
-              value="Login"
-              disabled={!login.length || !password.length}
-            />
-          </div>
-        </form>
-      </div>
-    </StyledLoginPage>
+            <div className="form__row form__row-fotter">
+              <Button
+                type="submit"
+                variant="contained"
+                disabled={!login.length || !password.length}
+              >
+                Login
+              </Button>
+            </div>
+          </form>
+        </div>
+      </StyledLoginPage>
+    </Container>
   );
 };
 
