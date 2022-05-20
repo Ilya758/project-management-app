d7e50a5f085fb5f7005f0e28631909a365d68447
inord777dev <inord777dev@gmail.com>
2022-05-20 16:58:35 +0300

    feat: add i18n

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import './i18n/config';
 import { Navigate, Route, Routes } from 'react-router-dom';
 import BoardPage from './components/BoardPage/BoardPage';
 import HomePage from './components/HomePage/HomePage';
