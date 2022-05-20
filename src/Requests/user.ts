381ffcf886cdf2d265eb52cabff452798f376323
tyzikova <tyzikova27@mail.ru>
2022-05-20 21:24:29 +0300

    fix:delete console.log

--- src/Requests/user.ts
+++ src/Requests/user.ts
@@ -6,6 +6,5 @@ export const editUser = () =>
   axios.get<IUser[]>(PATH.USERS).then((response) => {
     const login = localStorage.getItem('login');
     const user = response.data.find((elem) => elem.login === login);
-    console.log(response.data);
     return user;
   });
