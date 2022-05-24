4c351063801c653a7d9b46b7f60d6f98cf993063
Ilya758 <subscriber6436@gmail.com>
2022-05-24 21:06:53 +0300

    feat: implemented transition with sticky header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,10 +3,29 @@ import { Menu } from './MenuProfile';
 import { Authorization } from './Authorization';
 
 import './Header.scss';
+import { RefObject, useEffect, useRef } from 'react';
+import { Classnames } from './constants';
 
 export const Header = () => {
+  const headerRef = useRef(null) as RefObject<HTMLElement>;
+
+  useEffect(() => {
+    const headerElement = headerRef.current as HTMLElement;
+    const { height } = headerElement.getBoundingClientRect();
+
+    const handleScroll = () => {
+      scrollY <= height
+        ? headerElement.classList.remove(Classnames.headerOnScrollActive)
+        : headerElement.classList.add(Classnames.headerOnScrollActive);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => window.removeEventListener('scroll', handleScroll);
+  });
+
   return (
-    <header className="header">
+    <header ref={headerRef} className="header">
       <div className="nav-container">
         <NavLink to="/" className="logo"></NavLink>
         <Menu />
